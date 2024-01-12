517cb972f135bbdd64e6cb257644462ad615f61d
Jonathan Hanobik <Jonathan@Jonathans-MacBook-Pro.local>
2024-01-11 23:25:52 -0500

    Practice Files for Exercise 1 and 2

--- Exercise 1 - Variables and Methods/Runner.java
+++ Exercise 1 - Variables and Methods/Runner.java
@@ -3,35 +3,30 @@ public class Runner {
   //To Run and Test Your Changes, Open a Command Window and Make Sure Java is Installed
   //Issue Command: javac Runner.java (This Will Compile the Java To a Runnable File)
   //Issue Command: java -ea Runner (This Will Run the Runnable File with Assertions Enabled)
+  //Note: The static keyword is used to say that a method doesn't belong to a class instance.
 
   public static void main(String[] args) {
     // 1. Create two integer variables called num1 and num2 respectively. Initialize Their Values to 0.
-    int num1 = 0;
-    int num2 = 0;
 
     // 2. Change the value of num1 to 5 and the value of num2 to the value of two times num1.
-    num1 = 5;
-    num2 = 2 * num1;
 
     // 3. Call the method addInts() with x and y to get their sum. Store it in a variable called sum.
-    int sum = addInts(num1, num2);
+
     assert sum == 15 : "Wrong Value for num1 and num2!";
 
     // 4. Write a method below called isItEven() that takes one integer parameter called number.
     // In the method, return the boolean true if the number is even and false if the number is odd using conditional structures.
-    // Call the method within the System.out.println() and replace <method_call>.
+    // The method will be called within the assertion.
     assert isItEven(sum) == false : "Sum Should Not Be Even!";
 
     // 5. Initialize two double variables, one called tax and the other called price.
     // tax should be set to the decimal value of 7% (PA Sales Tax).
     // price should be set to the decimal value of price of an Azure Exam ($99.99).
-    double tax = 0.07;
-    double price = 99.99;
 
     // 6. Create a method that takes the tax and price and calculates what the customer should pay (including tax).
     // This method should be called costCalculator and should take the tax as the first parameter and price as the second parameter.
     // Store the result in a double called totalCost.
-    double totalCost = costCalculator(tax, price);
+
     assert totalCost == 106.9893 : "Wrong Total Price for Azure Exam!";
 
     System.out.println("Exercise 1 Success - Congrats!");
@@ -42,17 +37,9 @@ public class Runner {
   }
 
   // Method Implementation for Item 4
-  public static boolean isItEven(int number) {
-    if(number % 2 == 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+
 
   // Method Implementation for Item 5
-  public static double costCalculator(double tax, double price) {
-    return price + (tax * price);
-  }
+
 
 }
--- Exercise 2 - Class Interactions/Runner.java
+++ Exercise 2 - Class Interactions/Runner.java
@@ -8,16 +8,12 @@ public class Runner {
     Bike bicycle = new Bike();
 
     // 7. Use the Setters to Set Number of Wheels to 2, Color to "Blue", and hasMotor to false
-    bicycle.setNumberOfWheels(2);
-    bicycle.setColor("Blue");
-    bicycle.setHasMotor(false);
+
 
     // 8. Create another object with type Bike and variable named motorcycle. Use the constructor with all arguments.
-    Bike motorcycle = new Bike(2, "Blue", false);
 
     // 9. Use System.out.println() to call the .toString() method of each object.
-    System.out.println(bicycle.toString());
-    System.out.println(motorcycle.toString());
+
 
     // Verification Code - Do Not Modify
     assert bicycle.getNumberOfWheels() == 2 : "Bicycle has wrong number of wheels!";
